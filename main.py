fd4f38c8489c1f6e1d8a85f0edc462205d5e1273
Caleb <caleb@connolly.tech>
2019-02-24 16:32:03 +0000

    Fix error when message contains emoji NOW

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ async def on_message(message):
     if message.channel.id == objects["channels"]["suggestions"]:
         await handle_suggestion(message)
     msg = message.content
-    print(message.author.name+": "+msg.encode("utf8")))
+    print(message.author.name+": "+msg.encode("utf8"))
     if msg.startswith("$"):
         await bot.process_commands(message)
         return # Don't run spam filter on commands
